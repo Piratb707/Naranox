f008efebf17e2866e15ce3c0a0fd872ba13fa8aa
PiraTb707 <beloshapkobm@yandex.ru>
2023-01-17 18:26:56 -0300

    Старт , первый коммит.

--- main.py
+++ main.py
@@ -1,42 +1,80 @@
 import random
 
-class Player:
-    def __init__(self, name, health, power):
+class Character:
+    def __init__(self, name, level=1, hp=100, strength=10, agility=10, intelligence=10):
         self.name = name
-        self.health = health
-        self.power = power
-        self.gold = 0
-        self.level = 1
-        self.exp = 0
-        
-    def attack(self, enemy):
-        damage = self.power
-        enemy.health -= damage
-        print(f'{self.name} наносит {damage} урона {enemy.name}')
-        if enemy.health <= 0:
-            print(f'{enemy.name} погибает')
+        self.level = level
+        self.hp = hp
+        self.strength = strength
+        self.agility = agility
+        self.intelligence = intelligence
+        self.xp = 0
+        self.next_level_xp = 100
+        self.inventory = {}
+
+    # Повышение уровня 
     def level_up(self):
         self.level += 1
-        self.power += 10
-        print(f'{self.name} повышает уровень до {self.level}!')
+        self.hp += 10
+        self.strength += 2
+        self.agility += 2
+        self.intelligence += 2
+        self.next_level_xp *= 1.5
+        print(f"{self.name} повысил свой уровень до {self.level}!")
+    
+    # Получение опыта
+    def gain_xp(self, xp):
+        self.xp += xp
+        if self.xp >= self.next_level_xp:
+            self.level_up()
+            self.xp = 0
+        else:
+            print(f"{self.name} получено {xp} опыта. {self.next_level_xp - self.xp} Осталось опыта до следующего уровня.")
 
-def gain_exp(player, exp):
-    player.exp += exp
-    if player.exp >= 100:
-        player.exp -= 100
-        player.level_up()
+    # Получение повреждений 
+    def take_damage(self, damage):
+        self.hp -= damage
+        if self.hp <= 0:
+            print(f"{self.name} был побежден.")
+        else:
+            print(f"{self.name} получил {damage} единиц урона. {self.hp} Осталось здоровья.")
 
-player = Player("Игрок", 100, 20)
+    # Функция атаки
+    def attack(self, enemy):
+        damage = self.strength // 2
+        enemy.take_damage(damage)
+        print(f"{self.name} атакован {enemy.name} на {damage} единиц урона.")
+    
+    # Функция характеристик персонажа
+    def stats(self):
+        print(f"Имя: {self.name}")
+        print(f"Уровень: {self.level}")
+        print(f"Здоровье: {self.hp}")
+        print(f"Сила: {self.strength}")
+        print(f"Ловкость: {self.agility}")
+        print(f"Интелект: {self.intelligence}")
+        print(f"Опыт: {self.xp} / {self.next_level_xp}")
+        print("Инвентарь: ", self.inventory)
 
-while True:
-    print("1. Идти в путь")
-    print("2. Проверить состояние")
-    choice = input("Ваш ход: ")
-    if choice == "1":
-        exp = random.randint(10, 20)
-        print(f'Вы получаете {exp} опыта')
-        gain_exp(player, exp)
-    elif choice == "2":
-        print(f'Здоровье: {player.health}, Золото: {player.gold}, Уровень: {player.level}, Опыт: {player.exp}')
-    else:
-        print("Неверный выбор")
+# Функция создания персонажа (без класса)
+def create_character():
+    name = input("Как зовут вашего персонажа ")
+    return Character(name)
+
+player = create_character()
+player.stats()
+
+# Пример геймплея (тесты)
+enemy = Character("Гоблин", level=1, hp=50, strength=5, agility=5, intelligence=5)
+print("Впереди гоблин!")
+
+while enemy.hp > 0:
+    player.attack(enemy)
+    enemy.attack(player)
+    player.stats()
+    enemy.stats()
+
+player.gain_xp(50)
+print("Гоблин выронил меч , вы его подобрали.")
+player.inventory["меч"] = "Ржавый меч"
+player.stats()
